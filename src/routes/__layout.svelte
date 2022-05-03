064ea8332fb4cdb05ee6e66bd47a62454738f63d
Dheepak Krishnamurthy <me@kdheepak.com>
2022-05-02 22:48:43 -0600

    feat: Add links ✨

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,22 @@
   import "../app.scss";
 </script>
 
+<svelte:head>
+  <title>Dheepak Krishnamurthy - Portfolio</title>
+  <link rel="canonical" href="https://kdheepak.com/portfolio" />
+  <meta property="og:url" content="https://kdheepak.com/portfolio" />
+  <meta name="description" content="My Javascript HTML CSS SvelteKit portfolio" />
+  <meta name="robots" content="index, follow" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://kdheepak.com/portfolio" />
+  <meta property="og:title" content="My Javascript HTML CSS SvelteKit portfolio" />
+  <meta property="og:description" content="My Javascript HTML CSS SvelteKit portfolio" />
+  <meta property="og:site_name" content="kdheepak.com" />
+  <meta name="twitter:title" content="Dheepak Krishnamurthy - Portfolio" />
+  <meta name="twitter:description" content="My Javascript HTML CSS SvelteKit portfolio" />
+  <meta name="author" content="Dheepak Krishnamurthy" />
+</svelte:head>
+
 <div class="main">
   <slot />
 </div>
