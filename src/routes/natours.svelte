1654522c191eaae7b10e1cb83c3ffd676fdfb7a2
Dheepak Krishnamurthy <me@kdheepak.com>
2022-05-07 20:44:04 -0600

    feat: Create cool css animations ✨

--- src/routes/natours.svelte
+++ src/routes/natours.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main class="main">
-  <header class="relative h-[95vh]">
+  <header class="relative h-[95vh] hero-gradient bg-cover bg-top">
     <img
       class="absolute top-[40px] left-[40px] h-[35px]"
       src="{base}/images/logo-white.png"
@@ -12,24 +12,72 @@
     <h1
       class="absolute text-[#ffffff] uppercase top-[40%] left-1/2 translate-x-[-50%] translate-y-[-50%]"
     >
-      <span class="block font-bold text-[60px] tracking-[35px]">Outdoors</span>
-      <span class="block font-normal text-[20px] tracking-[17.4px]">is where life happens</span>
+      <span class="move-in-left block font-bold text-[60px] tracking-[35px]">Outdoors</span>
+      <span class="move-in-right block font-normal text-[20px] tracking-[17.4px]"
+        >is where life happens</span
+      >
     </h1>
   </header>
 </main>
 
 <style>
-  header {
-    background-image: linear-gradient(
-        to right bottom,
-        rgba(126, 213, 111, 0.8),
-        rgba(40, 180, 131, 0.8)
-      ),
-      url("../../static/images/hero.jpg");
-    background-size: cover;
-    background-position: top;
-    clip-path: polygon(0 0, 100% 0, 100% 80vh, 0 100%);
+  @tailwind components;
+
+  @layer components {
+    .hero-gradient {
+      /* from #7ed56f/80 to #28b483/80 */
+      background-image: linear-gradient(
+          to right bottom,
+          rgba(126, 213, 111, 0.8),
+          rgba(40, 180, 131, 0.8)
+        ),
+        url("../../static/images/hero.jpg");
+      clip-path: polygon(0 0, 100% 0, 100% 80vh, 0 100%);
+    }
+
+    .move-in-left {
+      animation-name: moveInLeft;
+      animation-duration: 1s;
+      animation-timing-function: ease-out;
+      backface-visibility: hidden;
+    }
+
+    .move-in-right {
+      animation-name: moveInRight;
+      animation-duration: 1s;
+      animation-timing-function: ease-out;
+      backface-visibility: hidden;
+    }
   }
+
+  @keyframes moveInLeft {
+    0% {
+      opacity: 0;
+      transform: translateX(-100px);
+    }
+    80% {
+      transform: translateX(10px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes moveInRight {
+    0% {
+      opacity: 0;
+      transform: translateX(100px);
+    }
+    80% {
+      transform: translateX(-10px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
   main {
     font-family: "Lato", sans-serif;
     font-weight: 400;
